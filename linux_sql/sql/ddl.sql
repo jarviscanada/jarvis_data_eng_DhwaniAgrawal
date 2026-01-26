45e394cc49b31845e20daab72e7503bd38fef11a
agrawaldhwani <dhwaniagrawal0@gmail.com>
2026-01-26 16:57:21 -0500

    Create host_info and host_usage tables

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,21 @@
+CREATE TABLE host_info (
+    id SERIAL PRIMARY KEY,
+    hostname VARCHAR(255) NOT NULL,
+    cpu_number INT NOT NULL,
+    cpu_architecture VARCHAR(50) NOT NULL,
+    cpu_model VARCHAR(255) NOT NULL,
+    cpu_mhz REAL NOT NULL,
+    l2_cache VARCHAR(50),
+    total_mem BIGINT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE host_usage (
+    id SERIAL PRIMARY KEY,
+    hostname VARCHAR(255) NOT NULL,
+    cpu_usage REAL NOT NULL,
+    mem_usage BIGINT NOT NULL,
+    disk_usage BIGINT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
